e6d04eaa43a5fa99b2c90314ca7f0872fbd8d290
hausol <hansolo6726@gmail.com>
2021-07-14 18:34:29 +0900

    donation, mypage(main, donation), event(main, detail), withdrawal make jsp

--- src/main/java/co/kr/petopia/controller/MemberController.java
+++ src/main/java/co/kr/petopia/controller/MemberController.java
@@ -33,4 +33,46 @@ public class MemberController {
 	public String jusoPopup() {
 		return "member/jusoPopup";
 	}
+	
+	// 마이페이지 메인
+	@GetMapping("/mypage")
+	public String mypage() {
+	    return "member/mypage";
+	}
+	// 마이페이지 기부
+	@GetMapping("/mypage_donation")
+    public String mypage_donation() {
+        return "member/mypage_donation";
+    }
+	
+	// 회원 탈퇴
+	@GetMapping("/withdrawal_agree")
+    public String withdrawal_agree() {
+        return "member/withdrawal_agree";
+    }
+	@GetMapping("/withdrawal")
+    public String withdrawal() {
+        return "member/withdrawal";
+    }
+	@GetMapping("/withdrawal_success")
+    public String withdrawal_success() {
+        return "member/withdrawal_success";
+    }
+	
+	// 이벤트 메인
+	@GetMapping("/event")
+    public String event() {
+        return "member/event";
+    }
+	// 이벤트 상세
+	@GetMapping("/event_detail")
+    public String event_detail() {
+        return "member/event_detail";
+    }
+	
+	// 기부 메인
+	@GetMapping("/donation")
+    public String donation() {
+        return "member/donation";
+    }
 }
