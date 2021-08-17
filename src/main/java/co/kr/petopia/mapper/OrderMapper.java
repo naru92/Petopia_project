e83283f4027a96c850de56e80ec1c7c90cced3df
66onewave <one_wave@naver.com>
2021-08-17 10:25:07 +0900

    petopia project

--- src/main/java/co/kr/petopia/mapper/OrderMapper.java
+++ src/main/java/co/kr/petopia/mapper/OrderMapper.java
@@ -5,27 +5,50 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-import co.kr.petopia.vo.CartVO;
-import co.kr.petopia.vo.OrderDetailVO;
 import co.kr.petopia.vo.OrderVO;
 
 @Mapper
-public interface OrderMapper{
+public interface OrderMapper {
+
+	// 회원
+	// 해당 멤버 정보 가지고오기
+	OrderVO getMemberInfoRead(String member_id);
 	
-	// 특정 회원의 장바구니 목록 가져오기
-	List<CartVO> getCartList();
+	// 회원의 장바구니 목록 가져오기
+	List<OrderVO> getMemberCartList(String member_id);
+
+	// 결제 시 정보 입력하기 메소드(order_form)
+	void memberOrderFormInsert(String member_id);
+
+	// 결제 시 정보 입력하기 메소드(order_detail)
+	void memberOrderDetailInsert(String member_id);
+
+	// order_form -> 주문번호 idx 불러오는 메소드
+	@Select("select order_seq.currval from dual")
+	int getMemberOrderFormCurrVal();
+
+	// 주문 상세 정보 가져오기
+	OrderVO readMemberOrderInfo(String member_id);
+
+	
+	
+	// 비회원
+	// 회원의 장바구니 목록 가져오기
+	
 
 	// 결제 시 정보 입력하기 메소드(order_form)
 	void orderFormInsert(OrderVO orderVO);
-	
+
 	// 결제 시 정보 입력하기 메소드(order_detail)
 	void orderDetailInsert(OrderVO orderVO);
-	
+
 	// order_form -> 주문번호 idx 불러오는 메소드
 	@Select("select order_seq.currval from dual")
 	int getOrderFormCurrVal();
 
+	// 결제 시 정보 입력하기 메소드(users)
+	void orderUserInsert(OrderVO orderVO);
+	
 	// 주문 상세 정보 가져오기
 	OrderVO readOrderInfo();
-	
 }
