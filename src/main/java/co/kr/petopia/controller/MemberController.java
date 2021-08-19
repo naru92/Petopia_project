67063425c45cec26d54c517934309188cf191166
AAAhrami <ahramshim@gmail.com>
2021-08-19 14:29:06 +0900

    detail modify

--- src/main/java/co/kr/petopia/controller/MemberController.java
+++ src/main/java/co/kr/petopia/controller/MemberController.java
@@ -340,7 +340,7 @@ public class MemberController {
         String member_id = principal.getName();
         model.addAttribute("replyList", memberService.getMyReplyList(member_id));
         
-        return "member/reviewList";
+        return "member/myReviewList";
     }
     
     // 후기 작성하기
@@ -390,7 +390,7 @@ public class MemberController {
         model.addAttribute("contentList", memberService.getMyContentList(member_id));
        
 
-        return "member/contentList";
+        return "member/myContentList";
     }
     
 
